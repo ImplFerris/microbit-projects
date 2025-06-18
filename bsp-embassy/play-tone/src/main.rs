870cf543c16087a80ec3ec39e5316ef7039cadac
ImplFerris <implferris@proton.me>
2025-06-18 08:14:16 +0530

    play tone

---
+++ bsp-embassy/play-tone/src/main.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::pwm::SimplePwm;
+use embassy_time::Timer;
+use microbit_bsp::{
+    Microbit,
+    speaker::{NamedPitch, Pitch, PwmSpeaker},
+};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let board = Microbit::default();
+
+    let mut button_a = board.btn_a;
+    let mut button_b = board.btn_b;
+
+    let mut speaker = PwmSpeaker::new(SimplePwm::new_1ch(board.pwm0, board.speaker));
+
+    loop {
+        button_a.wait_for_low().await;
+        speaker.start_note(Pitch::Named(NamedPitch::A4));
+        button_b.wait_for_low().await;
+        speaker.stop();
+    }
+
+    // loop {
+    //     if button_a.is_low() {
+    //         speaker.start_note(Pitch::Named(NamedPitch::A4));
+    //         Timer::after_millis(millis);
+    //     }
+    //     if button_b.is_low() {
+    //         speaker.stop();
+    //         Timer::after_millis(millis);
+    //     }
+    // }
+}
